341a492ea2286bc14342c90ef520987054a1cbf9
Alessandro-Fiora <fiora.ale97@gmail.com>
2024-11-20 14:22:14 +0100

    router

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-const posts = require("./db/posts");
-console.log(posts[0].title);
+const postsRouter = require("./routers/posts");
 
 // Asset statici
 app.use(express.static("public"));
@@ -14,31 +13,7 @@ app.get("/", (req, res) => {
   res.send("Server del mio Blog");
 });
 
-// Rotta Bacheca
-app.get("/bacheca", (req, res) => {
-  console.log("dashboard request received");
-
-  // FILTRO POST PER KEYWORD NEI TAGS
-  let filteredPosts = posts;
-
-  // SE C'E' UNA KEY DO A TERM IL VALORE DELLA KEY, ALTRIMENTI DO VALORE NULLO
-  const term = req.query.term ?? "";
-
-  // SE TERM HA UN VALORE FILTRO I POST PER QUELLI CHE INCLUDONO IL VALORE TRA I TAG
-  if (term) {
-    filteredPosts = posts.filter((post) => {
-      let isTermIncluded = false;
-      // PER OGNI POST CONTROLLO SE OGNI TAG CONTIENE TERM
-      post.tags.forEach((tag) => {
-        if (tag.toLowerCase().includes(term.toLowerCase()))
-          isTermIncluded = true;
-      });
-      return isTermIncluded;
-    });
-  }
-
-  res.json({ filteredPosts, postNumber: filteredPosts.length });
-});
+app.use("/posts", postsRouter);
 
 app.listen(port, () => {
   console.log("server listening on port", port);
---
+++ routers/posts.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const router = express.Router();
+
+const posts = require("../db/posts");
+
+// INDEX
+router.get("/", (req, res) => {
+  console.log("Richiesta index posts");
+
+  // FILTRO POST PER KEYWORD NEI TAGS
+  let filteredPosts = posts;
+
+  // SE C'E' UNA KEY DO A TERM IL VALORE DELLA KEY, ALTRIMENTI DO VALORE NULLO
+  const term = req.query.term ?? "";
+
+  // SE TERM HA UN VALORE FILTRO I POST PER QUELLI CHE INCLUDONO IL VALORE TRA I TAG
+  if (term) {
+    filteredPosts = posts.filter((post) => {
+      let isTermIncluded = false;
+      // PER OGNI POST CONTROLLO SE OGNI TAG CONTIENE TERM
+      post.tags.forEach((tag) => {
+        if (tag.toLowerCase().includes(term.toLowerCase()))
+          isTermIncluded = true;
+      });
+      return isTermIncluded;
+    });
+  }
+
+  res.json({ filteredPosts, postNumber: filteredPosts.length });
+});
+// SHOW
+router.get("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  res.json(`Dettagli del post ${id}`);
+});
+// STORE
+router.post("/", (req, res) => {
+  res.json("Crea nuovo post");
+});
+// UPDATE
+router.put("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  res.json(`Modifica integrale del post ${id}`);
+});
+// MODIFY
+router.patch("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  res.json(`Modifica parziale del post ${id}`);
+});
+// DESTROY
+router.delete("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  res.json(`Eliminazione del post ${id}`);
+});
+
+module.exports = router;
